02a6bfef7205b0195d0f39b306033eb2894d7a51
muktikt <rahayumukti200@gmail.com>
2026-06-02 20:57:57 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,19 @@ use App\Http\Controllers\GangguanController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PetugasController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/meter/ocr', [MeterController::class, 'ocr']);
 Route::post('/upload-meter', [MeterController::class, 'upload']);
+Route::get('/meter/history/{user_id}', [MeterController::class, 'history']);
 
 Route::get('/tagihan/{user_id}', [TagihanController::class, 'index']);
 
 Route::post('/pengaduan', [PengaduanController::class, 'store']);
+Route::get('/pengaduan/{user_id}', [PengaduanController::class, 'userHistory']);
 
 Route::get('/gangguan/{kecamatan}', [GangguanController::class, 'index']);
 
@@ -30,14 +33,8 @@ Route::post('/payment/cancel/{id}', [PaymentController::class, 'cancel']);
 Route::get('/profile/{id}', [UserProfileController::class, 'show']);
 Route::put('/profile/update/{id}', [UserProfileController::class, 'update']);
 
-Route::get('/meter/history/{user_id}', [MeterController::class, 'history']);
-
-Route::get('/pengaduan/{user_id}', [PengaduanController::class, 'userHistory']);
-
 Route::get('/notifikasi/{user_id}', [NotificationController::class, 'index']);
 Route::put('/notifikasi/read/{id}', [NotificationController::class, 'read']);
-
-use App\Http\Controllers\PetugasController;
 
 Route::post('/petugas/login', [PetugasController::class, 'apiLogin']);
 Route::get('/petugas/meter/{petugas_id}', [PetugasController::class, 'apiMeter']);
